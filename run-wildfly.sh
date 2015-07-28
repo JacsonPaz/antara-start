7eab452f840d8f05724c4597ce481fddf6ceee5f
JacsonPaz <jacsonpaz@systemhaus.com.br>
2015-07-28 15:19:42 -0300

    - corrigido executor - renomeado para suportar "jboss7"

---
+++ run-wildfly.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker pull 10.1.1.30:5000/wildfly 
+
+docker run -it -p 8080:8080 -p 8787:8787 \
+-v $PWD/deployments/:/antara/jboss/standalone/deployments/ \
+-v $PWD/standalone.xml:/antara/jboss/standalone/configuration/standalone.xml \
+-e "MEMORIA_MAXIMA=-Xmx1024m" \
+10.1.1.30:5000/wildfly
+
