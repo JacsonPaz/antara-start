098875d335eb975ac53ddda1dbb0302b4b4ab3ab
JacsonPaz <jacsonpaz@systemhaus.com.br>
2015-07-28 16:32:49 -0300

    Changes to be committed: 	modified:   run-jboss7.sh

--- run-jboss7.sh
+++ run-jboss7.sh
@@ -2,8 +2,9 @@
 docker pull 10.1.1.30:5000/jboss7 
 
 docker run -it -p 8080:8080 -p 8787:8787 \
--v $PWD/deployments/:/antara/jboss/standalone/deployments/ \
+-v $PWD/../../deployments/:/antara/jboss/standalone/deployments/ \
 -v $PWD/standalone.xml:/antara/jboss/standalone/configuration/standalone.xml \
+-v $PWD/antara-server-configuration.jar:/antara/jboss/standalone/deployments/Antara.ear/server.war/WEB-INF/lib/antara-server-configuration.jar \
 -e "MEMORIA_MAXIMA=-Xmx1024m" \
 10.1.1.30:5000/jboss7
 
