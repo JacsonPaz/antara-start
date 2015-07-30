defbfd9076294d15a19fbe21de1bc44f0b42a856
JacsonPaz <jacsonpaz@systemhaus.com.br>
2015-07-30 12:11:10 -0300

    modified:   dev/run-jboss7.sh 	modified:   dev/run-wildfly.sh

--- dev/run-jboss7.sh
+++ dev/run-jboss7.sh
@@ -3,8 +3,10 @@ docker pull 10.1.1.30:5000/jboss7
 
 docker run -it -p 8080:8080 -p 8787:8787 \
 -v $PWD/deployments/:/antara/jboss/standalone/deployments/ \
--v $PWD/standalone.xml:/antara/jboss/standalone/configuration/standalone.xml \
 -v $PWD/nfe/template:/antara/nfe/template \
 -e "MEMORIA_MAXIMA=-Xmx1024m" \
+-e "jboss_datasource_connectionurl=jdbc:postgresql://x.x.x.x:5432/banco" \
+-e "jboss_datasource_driver=driver.JAR ou jtds" \
+-e "jboss_datasource_username=login banco" \
+-e "jboss_datasource_password=senha banco" \
 10.1.1.30:5000/jboss7
-
--- dev/run-wildfly.sh
+++ dev/run-wildfly.sh
@@ -3,8 +3,10 @@ docker pull 10.1.1.30:5000/wildfly
 
 docker run -it -p 8080:8080 -p 8787:8787 \
 -v $PWD/deployments/:/antara/jboss/standalone/deployments/ \
--v $PWD/standalone.xml:/antara/jboss/standalone/configuration/standalone.xml \
 -v $PWD/nfe/template:/antara/nfe/template \
 -e "MEMORIA_MAXIMA=-Xmx1024m" \
+-e "jboss_datasource_connectionurl=driver://ip:porta/banco" \
+-e "jboss_datasource_driver=driver.jar ou jtds" \
+-e "jboss_datasource_username=login banco" \
+-e "jboss_datasource_password=senha banco" \
 10.1.1.30:5000/wildfly
-
