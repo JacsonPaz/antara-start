a03441e925d3e41c19bf497a7eb1bb52ff9fbaeb
JacsonPaz <jacsonpaz@systemhaus.com.br>
2015-07-30 13:55:21 -0300

    modified:   dev/run-jboss7.sh 	modified:   dev/run-wildfly.sh

--- dev/run-jboss7.sh
+++ dev/run-jboss7.sh
@@ -6,7 +6,7 @@ docker run -it -p 8080:8080 -p 8787:8787 \
 -v $PWD/nfe/template:/antara/nfe/template \
 -e "MEMORIA_MAXIMA=-Xmx1024m" \
 -e "jboss_datasource_connectionurl=jdbc:postgresql://x.x.x.x:5432/banco" \
--e "jboss_datasource_driver=driver.JAR ou jtds" \
+-e "jboss_datasource_driver=oracle OU postgresql OU jtds" \
 -e "jboss_datasource_username=login banco" \
 -e "jboss_datasource_password=senha banco" \
 10.1.1.30:5000/jboss7
--- dev/run-wildfly.sh
+++ dev/run-wildfly.sh
@@ -6,7 +6,7 @@ docker run -it -p 8080:8080 -p 8787:8787 \
 -v $PWD/nfe/template:/antara/nfe/template \
 -e "MEMORIA_MAXIMA=-Xmx1024m" \
 -e "jboss_datasource_connectionurl=driver://ip:porta/banco" \
--e "jboss_datasource_driver=driver.jar ou jtds" \
+-e "jboss_datasource_driver=jtds OU oracle OU postgresql" \
 -e "jboss_datasource_username=login banco" \
 -e "jboss_datasource_password=senha banco" \
 10.1.1.30:5000/wildfly
